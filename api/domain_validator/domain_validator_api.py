e821012cc70093b1f66e60ed35e13e0b2560b9cb
MariaCamarillo <mdelosangeles.camarillo@innovant.us>
2023-08-02 23:43:26 -0300

    feat:add api validacion de formato de correo y validacion de dominio en la blacklis hecho

--- api/domain_validator/domain_validator_api.py
+++ api/domain_validator/domain_validator_api.py
@@ -1,39 +1,47 @@
-from flask import Blueprint, request
-import db.database
+from flask import Blueprint, request, make_response, jsonify
+from db.database import supabaseConnection
 import re
-from supabase import create_client, Client
-import os
-
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
-supabase: Client = create_client(url, key)
 
 domain_validator = Blueprint('domain_validator', __name__)
 
 @domain_validator.route('/validate', methods=["POST"])
 def index():
     email = request.form['email']
-    if email != '' and validate_email_format(email):
-        return {"message": "valido"}
+    if email != '' and is_valid_email_format(email):
+        return make_response(jsonify({"error": True, "message":  "formato inválido"}), 400)
     else:
-        return {"message": "no valido"}
+        return make_response(jsonify({"error": False, "message":  "formato válido"}), 200)
 
-def validate_email_format(email):
-    #pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-    #pattern = r"^[a-zA-Z0-9_%+-]+(?:\.[a-zA-Z0-9_%+-]+)*@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
-    #pattern = r"^[a-zA-Z0-9_-]+(?:\.[a-zA-Z0-9_-]+)*@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
-    #pattern = r"^(?!.*[._-]{2})[a-zA-Z0-9_-]+(?:\.[a-zA-Z0-9_-]+)*@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+def is_valid_email_format(email):
     pattern = r"^(?!.*[._-]{2})(?!.*-\.|\.-|-_|__)[a-zA-Z0-9_-]+(?:\.[a-zA-Z0-9_-]+)*@[a-zA-Z0-9]+(?:[-.][a-zA-Z0-9]+)*\.(?!-)[a-zA-Z]{2,}$"
     if re.match(pattern, email):
         return True
     else:
         return False
+    
 
-@domain_validator.route('/insert-mail', methods=["POST"])
-def insert_mail():
+@domain_validator.route('/validate-domain', methods=["POST"])  
+def validate_domain():
     email = request.form['email']
-    data = insert_mail(email)
-    return data
+    validDomain = is_valid_domain(email)
+   
+    if not validDomain:
+        response = {"error": True, "message":  "dominio inválido"}
+        return  make_response(jsonify(response), 400)
+    
+    return  make_response(jsonify({"error": False, "message":  "dominio válido"}), 200)
 
-def insert_mail(email):
-    data = supabase.table("blacklist").insert({"mail_blacklist":email}).execute()
+def is_valid_domain(email):
+    emailDomain = email.partition('@')[2]
+    domainValidation = findInvalidDomain(emailDomain)
+    if len(domainValidation.data) > 0:
+        return False
+    
+    return True
+
+def insert_mail_blacklist(email):
+    data = supabaseConnection.table("blacklist").insert({"mail_blacklist":email}).execute()
+
+
+def findInvalidDomain(domain):
+    return supabaseConnection.table("blacklist").select("mail_blacklist").eq("mail_blacklist", domain).execute()
--- db/database.py
+++ db/database.py
@@ -3,8 +3,6 @@ import os
 
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
-supabase: Client = create_client(url, key)
+supabaseConnection: Client = create_client(url, key)
 
-def insert_mail(email):
-    data = supabase.table("blacklist").insert({"mail_blacklist":email}).execute()
 
