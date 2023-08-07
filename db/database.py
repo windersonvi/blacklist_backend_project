5ba6358427ea2a0d83a52c0035e4608260d9658a
MariaCamarillo <mdelosangeles.camarillo@innovant.us>
2023-08-06 22:20:35 -0300

    feat: validacion de nombre y telefono modificadas, se agrego blueprint

---
+++ db/database.py
@@ -0,0 +1,6 @@
+from supabase import create_client, Client
+import os
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabaseConnection: Client = create_client(url, key)
